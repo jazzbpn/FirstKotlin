63da7de3717131448182d0bd538ef3977c2d6500
dev_bpn <jazzbpn@gmail.com>
2018-02-06 12:24:45 +0545

    Kotlin View Binding

--- app/src/main/java/today/tootle/myapplication/MainActivity.kt
+++ app/src/main/java/today/tootle/myapplication/MainActivity.kt
@@ -2,14 +2,20 @@ package today.tootle.myapplication
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 /** TODO Learning */
 /** 1. Project Setup */
+/** 2. Initialize Github Repository and push project with .gitignore */
+/** 3. Kotlin View Binding using kotlin-android-extensions */
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        myTextView.setText("Set textView using kotlin synthetic view binding")
     }
 }
