7522cda82975a12333a1b06557d06abda3b20ba3
dev_bpn <jazzbpn@gmail.com>
2018-02-06 12:15:30 +0545

    Adding .gitignore and push files to github

---
+++ app/src/main/java/today/tootle/myapplication/MainActivity.kt
@@ -0,0 +1,15 @@
+package today.tootle.myapplication
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+/** TODO Learning */
+/** 1. Project Setup */
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
