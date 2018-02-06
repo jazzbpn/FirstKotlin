ac9297902303dbb9edc70dd39594f09f5e3c2e5d
dev_bpn <jazzbpn@gmail.com>
2018-02-06 12:28:36 +0545

    OnClick TextView

--- app/src/main/java/today/tootle/myapplication/MainActivity.kt
+++ app/src/main/java/today/tootle/myapplication/MainActivity.kt
@@ -2,6 +2,8 @@ package today.tootle.myapplication
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 
@@ -9,6 +11,7 @@ import kotlinx.android.synthetic.main.activity_main.*
 /** 1. Project Setup */
 /** 2. Initialize Github Repository and push project with .gitignore */
 /** 3. Kotlin View Binding using kotlin-android-extensions */
+/** 4. OnClick TextView*/
 
 class MainActivity : AppCompatActivity() {
 
@@ -17,5 +20,11 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         myTextView.setText("Set textView using kotlin synthetic view binding")
+
+        myTextView.setOnClickListener { view: View? ->
+
+            Toast.makeText(this, "OnClick testing in kotlin", Toast.LENGTH_LONG).show()
+
+        }
     }
 }
